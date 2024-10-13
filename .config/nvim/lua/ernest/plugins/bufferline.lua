cb46005f7088c13cbcf072ca9e253ca4c0938c13
dead-inside0 <ernest@levkovit.ch>
2024-10-13 12:31:23 +0200

    Bufferline update, removed transparency for kitty window

--- .config/nvim/lua/ernest/plugins/bufferline.lua
+++ .config/nvim/lua/ernest/plugins/bufferline.lua
@@ -1,42 +1,34 @@
 return {
-    "akinsho/bufferline.nvim",
-    version = "*",
-    dependencies = "nvim-tree/nvim-web-devicons",
-    config = function()
-        vim.opt.termguicolors = true
-        require("bufferline").setup({
-            options = {
-                diagnostics = "nvim_lsp",
-                numbers = function(ordinal, id)
-                    return ("%d:%f").format(id, ordinal)
-                end,
-                separator_style = "padded_slant",
-                buffer_close_icon = "󰅖",
-                modified_icon = "●",
-                close_icon = "",
-                offsets = {
-                    {
-                        filetype = "NvimTree",
-                        text = "File Explorer",
-                        text_align = "left",
-                        separator = true,
-                    },
-                },
-                indicator = {
-                    style = "underline",
-                },
-                hover = {
-                    enabled = true,
-                    delay = 100,
-                    reveal = { "close" },
-                },
-                highlights = {
-                    buffer_selected = {
-                        bold = true,
-                        italic = false,
-                    },
-                },
-            },
-        })
-    end,
+	"akinsho/bufferline.nvim",
+	version = "*",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	config = function()
+		vim.opt.termguicolors = true
+		require("bufferline").setup({
+			options = {
+				diagnostics = "nvim_lsp",
+				buffer_close_icon = "󰅖",
+				modified_icon = "●",
+				close_icon = "",
+				offsets = {
+					{
+						filetype = "NvimTree",
+						text = "File Explorer",
+						text_align = "left",
+					},
+				},
+				hover = {
+					enabled = true,
+					delay = 100,
+					reveal = { "close" },
+				},
+				highlights = {
+					buffer_selected = {
+						bold = true,
+						italic = false,
+					},
+				},
+			},
+		})
+	end,
 }
